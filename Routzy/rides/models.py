6d92503c293ed9f6e5ee2aa50b2f191a0ea50853
Harsh Thite <harshthite2003@gmail.com>
2024-11-24 21:27:28 +0530

    Initial commit.

--- Routzy/rides/models.py
+++ Routzy/rides/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class User(models.Model):
     name = models.CharField(max_length=100)
@@ -16,16 +17,16 @@ class Ride(models.Model):
         ('Auto', 'Auto'),
         ('Car', 'Car'),
     ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    vehicle_type = models.CharField(max_length=50, choices=VEHICLE_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Ride creator
     start_location = models.CharField(max_length=255)
     end_location = models.CharField(max_length=255)
+    vehicle_type = models.CharField(max_length=50)
     date_time = models.DateTimeField()
-    seats_available = models.IntegerField()
+    seats_available = models.PositiveIntegerField()
     cost_per_seat = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return f"{self.start_location} to {self.end_location}"
+        return f"{self.start_location} to {self.end_location} on {self.date_time}"
 
 class Booking(models.Model):
     ride = models.ForeignKey(Ride, on_delete=models.CASCADE)
--- Routzy/rides/views.py
+++ Routzy/rides/views.py
@@ -3,6 +3,8 @@ from .models import User, Ride, Booking
 from django.contrib.auth.hashers import make_password, check_password
 from django.http import JsonResponse
 from django.template.loader import render_to_string
+from django.contrib.auth.decorators import login_required
+
 
 # Home view (landing page)
 def home(request):
@@ -41,15 +43,34 @@ def signup(request):
     return render(request, 'signup.html')
 
 # Offer Ride view
+@login_required
 def offer_ride(request):
     if request.method == "POST":
-        # Handle form submission for offering a ride
-        pass
+        start_location = request.POST.get('start_location')
+        end_location = request.POST.get('end_location')
+        vehicle_type = request.POST.get('vehicle_type')
+        date_time = request.POST.get('date_time')
+        seats_available = request.POST.get('seats_available')
+        cost_per_seat = request.POST.get('cost_per_seat')
+
+        # Unwrap the SimpleLazyObject to get the actual user instance
+        user = User.objects.get(pk=request.user.id)
+
+        Ride.objects.create(
+            user=user,
+            start_location=start_location,
+            end_location=end_location,
+            vehicle_type=vehicle_type,
+            date_time=date_time,
+            seats_available=seats_available,
+            cost_per_seat=cost_per_seat
+        )
+        return redirect('rides_list')  # Redirect to Available Rides page
     return render(request, 'offer_ride.html')
 
 # List available rides
 def rides_list(request):
-    rides = Ride.objects.all()
+    rides = Ride.objects.all()  # Query all rides
     return render(request, 'rides_list.html', {'rides': rides})
 
 # Book a ride
@@ -67,3 +88,6 @@ def filter_rides(request):
 
     html = render_to_string('partials/rides_list.html', {'rides': filtered_rides})
     return JsonResponse({'html': html})
+
+def chat_room(request, room_name):
+    return render(request, 'chat.html', {'room_name': room_name})
