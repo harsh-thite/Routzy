693d4025a635b434f8753d4a89ea0a4f767d9432
Harsh Thite <harshthite2003@gmail.com>
2024-12-07 23:13:05 +0530

    Some updates.

--- routzy/accounts/urls.py
+++ routzy/accounts/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     # Login Page (using Django's built-in LoginView)
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
 
-    # Logout' (using Django's built-in Logout-View)
+    # Logout (using Django's built-in Logout-View)
     path('logout/', LogoutView.as_view(next_page='home'), name='logout'),  # Redirects to home after logout
 
     # Signup' Page (Custom Signup View)
