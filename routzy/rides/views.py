5f1b1c54465f96dccf8cdaf068f35660bab78440
Harsh Thite <harshthite2003@gmail.com>
2024-12-12 19:22:46 +0530

    Some updates.

--- routzy/rides/views.py
+++ routzy/rides/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Vehicle, Ride, RideBooking
+from .models import Vehicle, Ride, Booking
 from .forms import RideOfferForm, VehicleForm, ChatMessageForm
 from django.contrib import messages
 
