63bf2755981e9a7609c303b4605a363eaa9c71e6
Harsh Thite <harshthite2003@gmail.com>
2024-12-12 19:27:48 +0530

    Some updates.

--- routzy/rides/admin.py
+++ routzy/rides/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Vehicle, Ride, RideBooking
+from .models import Vehicle, Ride, Booking, ChatMessage
 
+# Register your models here.
 admin.site.register(Vehicle)
 admin.site.register(Ride)
-admin.site.register(RideBooking)
+admin.site.register(Booking)
+admin.site.register(ChatMessage)
