a918045c621db6127bca4e488e1fe28d2a684ad1
Harsh Thite <harshthite2003@gmail.com>
2024-12-12 19:33:09 +0530

    Some updates.

--- routzy/rides/views.py
+++ routzy/rides/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Vehicle, Ride, Booking
+from .models import Vehicle, Ride, Booking, ChatMessage
 from .forms import RideOfferForm, VehicleForm, ChatMessageForm
 from django.contrib import messages
 
@@ -23,9 +23,11 @@ def offer_ride(request):
         vehicle_form = VehicleForm()
     return render(request, 'rides/offer_ride.html', {'ride_form': ride_form, 'vehicle_form': vehicle_form})
 
+
 def available_rides(request):
     rides = Ride.objects.filter(status='OPEN')
     return render(request, 'rides/available_rides.html', {'rides': rides})
+
 
 @login_required
 def ride_details(request, ride_id):
@@ -40,8 +42,9 @@ def ride_details(request, ride_id):
             return redirect('ride_details', ride_id=ride.id)
     else:
         message_form = ChatMessageForm()
-    messages = ride.messages.all()
-    return render(request, 'rides/ride_details.html', {'ride': ride, 'messages': messages, 'message_form': message_form})
+    messages_list = ride.messages.all()
+    return render(request, 'rides/ride_details.html', {'ride': ride, 'messages': messages_list, 'message_form': message_form})
+
 
 @login_required
 def book_ride(request, ride_id):
@@ -55,20 +58,12 @@ def book_ride(request, ride_id):
         messages.error(request, "No available seats for this ride.")
     return redirect('available_rides')
 
+
 @login_required
 def my_rides(request):
     offered_rides = request.user.offered_rides.all()
-    booked_rides = Booking.objects.filter(passenger=request.user)
+    booked_rides = Booking.objects.filter(user=request.user)
     return render(request, 'rides/my_rides.html', {'offered_rides': offered_rides, 'booked_rides': booked_rides})
-
-def ride_chat(request, ride_id):
-    ride = get_object_or_404(Ride, id=ride_id)  # Get the Ride object
-    chat_messages = ChatMessage.objects.filter(ride_id=ride_id).order_by('timestamp')  # Fetch chat messages for this ride
-
-    return render(request, 'rides/ride_chat.html', {
-        'ride': ride,
-        'chat_messages': chat_messages
-    })
 
 
 @login_required
