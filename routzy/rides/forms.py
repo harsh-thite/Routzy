c7dfb9ba56eed0a5eb836dc7a6d879a68650207f
Harsh Thite <harshthite2003@gmail.com>
2024-12-25 21:26:28 +0530

    Some updates.

--- routzy/rides/forms.py
+++ routzy/rides/forms.py
@@ -2,6 +2,11 @@ from django import forms
 from .models import Ride, Vehicle, Message
 
 class RideOfferForm(forms.ModelForm):
+    departure_time = forms.DateTimeField(
+        widget=forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+        input_formats=['%Y-%m-%dT%H:%M'],
+    )
+
     class Meta:
         model = Ride
         fields = ['start_location', 'end_location', 'departure_time', 'total_seats', 'cost_per_seat']
