dd22535a4d1f26ba89460e35b9f52b0cd4f08dd0
Harsh Thite <harshthite2003@gmail.com>
2025-01-14 16:42:14 +0530

    Some updates.

--- routzy/accounts/urls.py
+++ routzy/accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),  # Added `next_page`
     path('signup/', views.signup, name='signup'),
     path('profile/', views.profile, name='profile'),
 ]
